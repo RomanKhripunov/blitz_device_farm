fab3ad3d81284568f6c24d82edcf34959deab578
Roman Khripunov <roma.khripunov@gmail.com>
2018-07-10 10:24:24 +0300

    change environment

---
+++ ENV_devices_farm/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/r_khripunov/PycharmProjects/blitz_device_farm/ENV_devices_farm/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- bdf_env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/r_khripunov/PycharmProjects/blitz_device_farm/bdf_env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
